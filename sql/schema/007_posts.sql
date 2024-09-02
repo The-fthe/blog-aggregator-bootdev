cc623f2d6e36f1c8fcfb88cf8bdb8433a65fbf97
fthe <lcslianandfthe.cs@gmail.com>
2024-09-03 00:57:56 +0900

    feature: added post with create and getPost queue

---
+++ sql/schema/007_posts.sql
@@ -0,0 +1,14 @@
+-- +goose Up
+CREATE TABLE posts(
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  title VARCHAR(255) NULL,
+  url VARCHAR(2083) UNIQUE,
+  description TEXT NULL,
+  published_at TIMESTAMP NULL,
+  feed_id UUID NOT NULL REFERENCES feeds(id) ON DELETE CASCADE
+);
+
+-- +goose Down
+DROP TABLE posts;
